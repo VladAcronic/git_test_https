0c724357bcf56e8b5c84ea03f8b96c0085cab988
Acronic <acronic83@gmail.com>
2021-10-15 03:28:01 +0700

    === test commit file ===

---
+++ main script.py
@@ -0,0 +1,10 @@
+# this is nowel file for testing prnt func
+
+# prine empty line
+print ()
+
+# print some string
+print ("file was modified under tracking")
+
+# also print some string:
+print ("string for presicion tracking diff")
